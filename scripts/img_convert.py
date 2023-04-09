d9e75e0802e4be4308d1d1edf1623c16ce03026b
krishnansr <krishnansr.siva@gmail.com>
2023-04-09 18:12:36 -0400

    script init

--- scripts/img_convert.py
+++ scripts/img_convert.py
@@ -1,12 +1,43 @@
-from pdf2image import convert_from_path, convert_from_bytes
+import os
+import PIL
+import json
+import pathlib
+
+from pdf2image import convert_from_path
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
     PDFPageCountError,
     PDFSyntaxError
 )
 
+def create_if_empty(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return True
 
 if __name__ == '__main__':
-    pdf_path = '../dataset/ikea_man/pdfs/Bench/applaro/0.pdf'
-    images = convert_from_path(pdf_path)
-    print(type(images[0]))
+    pdf_paths = list(pathlib.Path('../dataset/ikea_man/pdfs').glob('**/*.pdf'))
+    create_if_empty("../dataset/images")
+
+    ind_hashmap = {}
+    running_ctr = 0
+
+    for pdf_path in pdf_paths[:3]:
+        *_, class_name, model_name = str(pdf_path.parent).rsplit('/')
+        images = convert_from_path(pdf_path)
+
+        for img in images:
+            w, h = img.size
+            img_name = f"{running_ctr:04d}.png"
+            img.save(f"../dataset/images/{img_name}")
+            ind_hashmap[img_name] = {
+                "class_name": class_name,
+                "model_name": model_name,
+                "pdf_path": str(pdf_path),
+                "img_h": h,
+                "img_w": w,
+            }
+            running_ctr += 1
+
+    with open("../dataset/ind_map.json", "w") as f:
+        json.dump(ind_hashmap, f, indent=6)
