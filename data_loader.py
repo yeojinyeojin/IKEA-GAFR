5eaaa459ad426e246abc7e7b6c9932b4193cc926
Yeojin Jung <yj1254@nyu.edu>
2023-04-10 09:29:36 -0400

    debugged & confirmed pix2voxel training running

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,7 @@ import argparse
 from glob import glob
 
 import cv2
+from PIL import Image
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -13,7 +14,7 @@ from torchvision import models as torchvision_models
 import pytorch3d
 
 class IKEAManual(Dataset):
-    def __init__(self, datadir):
+    def __init__(self, datadir, load_feat=False, transforms=None):
         super().__init__()
         
         self.imgdir = f"{datadir}/images"
@@ -21,12 +22,31 @@ class IKEAManual(Dataset):
         
         self.imgs = []
         for imgpath in self.imgpaths:
-            img = cv2.imread(imgpath)
+            img = Image.open(imgpath)
+            if transforms is not None:
+                img = transforms(img)
             self.imgs.append(img)
         # sorted(self.imgs) #TODO: needed?
+        
+        #TODO: load voxels
+        self.voxels = self.imgs
+        # self.voxels = []
+        
+        self.load_feat = load_feat
+        if load_feat:
+            self.feats = torch.from_numpy(np.load(f"{datadir}/feats.npy"))
         
     def __len__(self):
         return len(self.imgs)
 
     def __getitem__(self, idx):
-        return self.imgs[idx]
+        dic = {
+            'images': self.imgs[idx],
+            'voxels': self.voxels[idx],
+        }
+        
+        if self.load_feat:
+            dic['feats'] = self.feats[idx]
+        
+        return dic
+        
