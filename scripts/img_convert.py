0464d52260cc3dc98d9e1d6ff51dc41b9311ad54
krishnansr <krishnansr.siva@gmail.com>
2023-04-10 19:11:33 -0400

    vgg annot changes

--- scripts/img_convert.py
+++ scripts/img_convert.py
@@ -10,21 +10,29 @@ from pdf2image.exceptions import (
     PDFSyntaxError
 )
 
+
 def create_if_empty(path):
     if not os.path.exists(path):
         os.mkdir(path)
     return True
 
+
 if __name__ == '__main__':
-    pdf_paths = list(pathlib.Path('../dataset/ikea_man/pdfs').glob('**/*.pdf'))
+    pdf_paths = sorted(list(pathlib.Path('../dataset/ikea_man/pdfs').glob('**/*.pdf')))
     create_if_empty("../dataset/images")
 
     ind_hashmap = {}
     running_ctr = 0
 
+    _prev_class_name = ''
+    _prev_model_name = ''
+    _prev_page_end = 0
     for pdf_path in pdf_paths:
         *_, class_name, model_name = str(pdf_path.parent).rsplit('/')
         images = convert_from_path(pdf_path)
+
+        if class_name != _prev_class_name or model_name != _prev_model_name:
+            _prev_page_end = 0
 
         for i, img in enumerate(images):
             w, h = img.size
@@ -37,8 +45,14 @@ if __name__ == '__main__':
                 "img_h": h,
                 "img_w": w,
                 "pdf_page_id": i,
+                "model_page_id": i + _prev_page_end,
+                "pdf_name": pdf_path.name,
             }
             running_ctr += 1
+
+        _prev_class_name = class_name
+        _prev_model_name = model_name
+        _prev_page_end = i + 1
 
     with open("../dataset/ind_map.json", "w") as f:
         json.dump(ind_hashmap, f, indent=6)
