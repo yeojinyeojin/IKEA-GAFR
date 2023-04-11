29918b6065982cd6c8f0c58eb0f42ba15996df57
niviruwijayaratne <nnw2@illinois.edu>
2023-04-11 18:23:17 -0400

    added .obj -> .off file conversion, cleaned up code, updated data README

--- scripts/README.md
+++ scripts/README.md
@@ -1,8 +1,16 @@
 ### Getting CAD Model Ground Truths
 Run the following from this directory:
 ```python
-python cad_model.py --dataset_path <PATH TO IKEA-MANUAL DATASET> --output_dir <DIRECTORY TO WRITE CAD MODELS TO>
+python cad_model.py --args 
 ```
+
+Arguments:
+- `--dataset_path`: Path to IKEA-Manual Dataset
+- `--output_dir`: Directory to save CAD Models
+- `--save_obj_files`: Include if you want to save models in .OBJ format
+- `--save_off_files`: Include if you want to save models in .OFF format (.OFF models used for mesh voxelization)
+- `--visualize`: Include if you want to visualize each sample
+
 The script will create the output directory if it does not exist and writes step-wise CAD models following the same file structure as the IKEA-Manual dataset ie.
 ```
 <output_dir>
@@ -15,7 +23,20 @@ The script will create the output directory if it does not exist and writes step
 |   |    -- step_3.obj
 |   |    -- step_4.obj
 |   |    -- step_5.obj
-|   |-- appalro
+|   |-- applaro
 |   ...
 | ...
+```
+
+The above structure is purely for easy reference. For training, the script will save numbered step-wise .OBJ models under a `obj_models/` directory within your dataset directory. It will also save numbered step-wise .OFF models under a `off_models/` directory within your dataset directory. ie.
+```
+<dataset dir>
+|-- obj_models
+|   |-- 00000.obj
+|   |-- 00001.obj
+|   ...
+|-- off_models
+|   |-- 00000.off
+|   |-- 00001.off
+|   ...
 ```
--- scripts/cad_model.py
+++ scripts/cad_model.py
@@ -1,13 +1,17 @@
 import json
 import os
 import torch
+import argparse as ap
+import shutil
+import numpy as np
+import cv2
+
 from pytorch3d.structures import Meshes
 from pytorch3d.io import load_obj, save_obj
 from pytorch3d.transforms import Rotate, Translate, matrix_to_euler_angles
 from pytorch3d.renderer import TexturesVertex, look_at_view_transform
 from pytorch3d.renderer.cameras import FoVPerspectiveCameras
 from pytorch3d.vis.plotly_vis import plot_scene
-import argparse as ap
 
 # p3d_coordinate_frame = Rotate(torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
 # opencv_coordinate_frame = Rotate(torch.tensor([[-1, 0, 0], [0, -1, 0], [0, 0, 1]]))
@@ -32,54 +36,81 @@ def preprocess_part_idxs(part_list):
     
     return step_part_nums, step_part_idxs
 
+def obj_to_off(verts, faces, dataset_dir, model_num):
+    print("Saving model {:05d} as OFF file".format(model_num))
+    numbers_data = torch.tensor([[len(verts), len(faces), 0]], dtype=int).cpu().numpy()
+    vert_data = verts.cpu().numpy()
+    face_data = torch.cat([torch.ones((faces.shape[0], 1))*faces.shape[1], faces], dim = 1).to(int).cpu().numpy()
+    tmp_dir = "./tmp"
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+
+    if not os.listdir(tmp_dir):
+        print("HERE")
+        with open(os.path.join(tmp_dir, "off.txt"), 'w') as f:
+            f.write("OFF")
+        f.close()
+
+    np.savetxt(os.path.join(tmp_dir, "numbers.txt"), numbers_data, fmt="%d")
+    np.savetxt(os.path.join(tmp_dir, "verts.txt"), vert_data)
+    np.savetxt(os.path.join(tmp_dir, "faces.txt"), face_data, fmt="%d")
+    output_dir = os.path.join(dataset_dir, "off_models")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    off_path = os.path.join(tmp_dir, "off.txt")
+    numbers_path = os.path.join(tmp_dir, "numbers.txt")
+    verts_path = os.path.join(tmp_dir, "verts.txt")
+    faces_path = os.path.join(tmp_dir, "faces.txt")
+
+    with open(os.path.join(output_dir, "{:05d}.off".format(model_num)),'wb') as out_file:
+        for f_num, f in enumerate([off_path, numbers_path, verts_path, faces_path]):
+            with open(f,'rb') as fd:
+                shutil.copyfileobj(fd, out_file)
+                if f_num == 0:
+                    out_file.write(b"\n")
+
 def main(args):
     dataset_path = args.dataset_path
     data_json = open(os.path.join(dataset_path, "main_data.json"))
     data = json.load(data_json)
 
     counter = 0
-    for sample_num, sample in enumerate(data):
+    for sample in data:
         category = sample['category']
         name = sample['name']
-        print(f'{category}: {name}')
+        print(f'Processing {category}: {name}...')
         model_dir = os.path.join(dataset_path, "parts", category, name)
         part_names = sorted(os.listdir(model_dir))
-        # prev_part_transforms = [None]*len(part_names)
+        if args.prediction_frame == "object-centric":
+            prev_part_transforms = [None]*len(part_names)
         
         for step_num, step_data in enumerate(sample['steps']):
-            print(f'Step {step_num}')
-            # extrinsics = step_data['extrinsics']
-            # intrinsics = step_data['intrinsics']
             model_verts = None
             model_faces = None
             
             step_part_nums, step_part_idxs = preprocess_part_idxs(step_data['parts'])
-            # print(f'Step {step_num}')
-            # print(f'Part Nums {step_part_nums}')
-            # print(f'Part Idxs {step_part_idxs}')
+            extrinsics = step_data['extrinsics']
+            intrinsics = step_data['intrinsics']
+
             for part_idx, part_num in zip(step_part_idxs, step_part_nums):
                 part_num = int(part_num)
                 part_path = os.path.join(model_dir, part_names[part_num]) # we want to index into part_names with the model_number as that will be the correct index
-                # part_extrinsics = torch.tensor(extrinsics[part_idx]) # we want to index into extrinsics in the same order the parts are presented
-                # print((matrix_to_euler_angles(part_extrinsics[:-1, :-1].unsqueeze(0), convention="XYZ")))
-                # print(torch.rad2deg(matrix_to_euler_angles(part_extrinsics[:-1, :-1].unsqueeze(0), convention="XYZ")))
-                # continue
-                # part_R = Rotate(part_extrinsics[:-1, :-1]) #Note coordinate system and how transformations are applied, check under Transform3D class https://pytorch3d.readthedocs.io/en/latest/modules/transforms.html
-                # part_t = Translate(part_extrinsics[:-1, -1].reshape(-1, 3)) #Note coordinate system and how transformations are applied, check under Transform3D class https://pytorch3d.readthedocs.io/en/latest/modules/transforms.html
-                
-                # part_t = Translate(torch.fliplr((part_extrinsics[:-1, -1]/100.0).reshape(-1, 3))) #Divide by 100 (unit issue) and flip (coordinate system issue)???????                
-                # part_t = Translate(((part_extrinsics[:-1, -1]/100.0).reshape(-1, 3)))
-
-                # if step_num == 0:
-                #     part_Rt = part_R.compose(part_t) #rotate then translate
-                # else:
-                #     if prev_part_transforms[part_num] is not None:
-                #         part_Rt = prev_part_transforms[part_num].compose(part_R).compose(part_t) #compose with previous transform
-                
-                # prev_part_transforms[part_num] = part_Rt
                 part_verts, part_face_data, _ = load_obj(part_path)
-                # part_verts = part_Rt.transform_points(part_verts)
                 part_faces = part_face_data.verts_idx
+                if args.prediction_frame == "object-centric":
+                    part_extrinsics = torch.tensor(extrinsics[part_idx]) # we want to index into extrinsics in the same order the parts are presented
+                    part_R = Rotate(part_extrinsics[:-1, :-1]) #Note coordinate system and how transformations are applied, check under Transform3D class https://pytorch3d.readthedocs.io/en/latest/modules/transforms.html
+                    part_t = Translate(torch.fliplr((part_extrinsics[:-1, -1]/100.0).reshape(-1, 3))) #Divide by 100 (unit issue) and flip (coordinate system issue)???????                
+
+                    if step_num == 0:
+                        part_Rt = part_R.compose(part_t) #rotate then translate
+                    else:
+                        if prev_part_transforms[part_num] is not None:
+                            part_Rt = prev_part_transforms[part_num].compose(part_R).compose(part_t) #compose with previous transform
+                    
+                    prev_part_transforms[part_num] = part_Rt
+                    part_verts = part_Rt.transform_points(part_verts)
                 
                 if model_verts is None:
                     model_verts = part_verts
@@ -88,47 +119,50 @@ def main(args):
                     part_faces += len(model_verts)
                     model_verts = torch.cat([model_verts, part_verts], dim = 0)
                     model_faces = torch.cat([model_faces, part_faces], dim = 0)
-            # return
 
-            # model_textures = torch.ones_like(model_verts)*torch.tensor([0.7, 0.7, 1.0])
-            # model_mesh = Meshes(
-            #     verts=model_verts.unsqueeze(0),
-            #     faces=model_faces.unsqueeze(0),
-            #     textures=TexturesVertex(model_textures.unsqueeze(0))
-            # )
-            model_write_dir = os.path.join(args.output_dir, category, name + "/")
-            print(f'Writing model to {model_write_dir}')
-            if not os.path.exists(model_write_dir):
-                os.makedirs(model_write_dir)
-            save_obj(f=os.path.join(model_write_dir, "step_" + str(step_num) + ".obj"), verts=model_verts, faces=model_faces)
+            if args.save_obj_files:
+                model_write_dir = os.path.join(args.output_dir, category, name + "/")
+                print(f'Writing model to {model_write_dir}')
+                if not os.path.exists(model_write_dir):
+                    os.makedirs(model_write_dir)
+                save_obj(f=os.path.join(model_write_dir, "step_" + str(step_num) + ".obj"), verts=model_verts, faces=model_faces)
+                
+                model_write_dir = os.path.join(os.path.join(args.dataset_path), "models" + "/")
+                if not os.path.exists(model_write_dir):
+                    os.makedirs(model_write_dir)
+                save_obj(f=os.path.join(model_write_dir, "{:05d}".format(counter) + '.obj'), verts=model_verts, faces=model_faces)
             
-            model_write_dir = os.path.join(os.path.join(args.dataset_path), "models" + "/")
-            if not os.path.exists(model_write_dir):
-                os.makedirs(model_write_dir)
-            save_obj(f=os.path.join(model_write_dir, "{:05d}".format(counter) + '.obj'), verts=model_verts, faces=model_faces)
+            if args.save_off_files:
+                obj_to_off(model_verts, model_faces, dataset_path, counter)
+            
             counter += 1
-            
-        #     cam_R, cam_T = look_at_view_transform(
-        #         dist=3,
-        #         elev=0,
-        #         azim=torch.linspace(-180.0, 180.0, 10),
-        #     )
 
-        #     cameras = FoVPerspectiveCameras(R=cam_R, T=cam_T)
+            if args.visualize:
+                model_textures = torch.ones_like(model_verts)*torch.tensor([0.7, 0.7, 1.0])
+                model_mesh = Meshes(
+                    verts=model_verts.unsqueeze(0),
+                    faces=model_faces.unsqueeze(0),
+                    textures=TexturesVertex(model_textures.unsqueeze(0))
+                )
+                scene = plot_scene({
+                    "Figure1": {
+                        "Meshes": model_mesh
+                    }
+                })
+                scene.show()
+                input("Close viz window and press enter to continue to next sample...")
 
-        #     scene = plot_scene({
-        #         "Figure1": {
-        #             "Meshes": model_mesh
-        #         }
-        #     })
-        #     scene.show()
-        #     break
-        # break
+    if args.save_off_files:
+        shutil.rmtree("./tmp")
     print("Done getting all CAD models")
 
 if __name__ == '__main__':
     parser = ap.ArgumentParser()
     parser.add_argument('--dataset_path', type=str, help='Path to IKEA-Manual Dataset')
     parser.add_argument('--output_dir', type=str, help="Directory to write CAD models to", default="./cad_models")
+    parser.add_argument('--prediction_frame', type=str, help="Either 'canonical' or 'object-centric'", default="canonical")
+    parser.add_argument('--save_off_files', action=ap.BooleanOptionalAction)
+    parser.add_argument('--save_obj_files', action=ap.BooleanOptionalAction)
+    parser.add_argument('--visualize', action=ap.BooleanOptionalAction)
     args = parser.parse_args()
     main(args)
