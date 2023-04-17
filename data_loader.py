output datastructure of rotation & edge contouring fixed; pix2vox training pipeline adjusted to pretrain with r2n2 edge data

data_loader.py
@@ -31,7 +31,7 @@ from pytorch3d.datasets.r2n2.utils import (
 import utils_vox
 
 def read_hdf5(file, key = 'tensor'):
-    assert os.os.path.exists(file), 'file %s not found' % file
+    assert os.path.exists(file), 'file %s not found' % file
     h5f = h5py.File(file, 'r')
     assert key in h5f.keys(), 'key %s not found in file %s' % (key, file)
     gt_voxels = torch.from_numpy(h5f[key][()])
@@ -81,41 +81,41 @@ class IKEAManualStep(Dataset):
         datadir = args.dataset_path
         transforms = args.transforms
 
-        self.imgdir = os.os.path.join(datadir, "images") #Change to images/rgb/ later
+        self.imgdir = os.path.join(datadir, "images") #Change to images/rgb/ later
         self.imgpaths = sorted(os.listdir(self.imgdir))
-        self.img_metadata = json.load(open(os.os.path.join(datadir, "ind_map.json"))) #Gives image height/width and other useful info
+        self.img_metadata = json.load(open(os.path.join(datadir, "ind_map.json"))) #Gives image height/width and other useful info
 
-        self.labeldir = os.os.path.join(datadir, "labels")
+        self.labeldir = os.path.join(datadir, "labels")
         self.labelpaths = sorted(os.listdir(self.labeldir))
 
         if args.use_line_seg: #If we want to concatenate line segmentations to input
-            self.line_seg_dir = os.os.path.join(datadir, "images", "line_seg")
+            self.line_seg_dir = os.path.join(datadir, "images", "line_seg")
             self.line_seg_paths = sorted(os.listdir(self.line_seg_dir))
         
         if args.use_seg_mask: #If we want to concatenate segmentation masks to input
-            self.seg_mask_dir = os.os.path.join(datadir, "images", "mask")
+            self.seg_mask_dir = os.path.join(datadir, "images", "mask")
             self.seg_mask_paths = sorted(os.listdir(self.seg_mask_dir))
 
 
-        self.gt_voxels = read_hdf5(os.os.path.join(datadir, "off_models_32_x_32", "output.h5"))
+        self.gt_voxels = read_hdf5(os.path.join(datadir, "off_models_32_x_32", "output.h5"))
 
         self.imgs = []
         self.imgnums = []
 
         for imgpath, labelpath in zip(self.imgpaths, self.labelpaths):
-            img = cv2.imread((os.os.path.join(self.imgdir, imgpath)))
+            img = cv2.imread((os.path.join(self.imgdir, imgpath)))
 
             if args.use_line_seg:
-                line_seg_img = cv2.imread(os.os.path.join(self.line_seg_dir, imgpath))
+                line_seg_img = cv2.imread(os.path.join(self.line_seg_dir, imgpath))
                 img = np.dstack([img, line_seg_img]) #Stack RGB img and line segmentation depth-wise
             if args.use_seg_mask:
-                seg_mask_img = cv2.imread(os.os.path.join(self.seg_mask_dir, imgpath))
+                seg_mask_img = cv2.imread(os.path.join(self.seg_mask_dir, imgpath))
                 img = np.dstack([img, seg_mask_img]) #Stack RGB img and segmentation mask depth-wise
 
             img_data = self.img_metadata[imgpath]
             h, w = img_data["img_h"], img_data["img_w"]
 
-            with open(os.os.path.join(self.labeldir, labelpath)) as f: #Read in label txt file for specific image
+            with open(os.path.join(self.labeldir, labelpath)) as f: #Read in label txt file for specific image
                 for line in f.readlines(): #For each bbox in image
                     line = np.array(line.split(" ")).astype(np.float32).reshape(-1) # [class, x_c, y_c, w, h]
                     line = torch.from_numpy(line)
@@ -152,8 +152,8 @@ class IKEAManualStep(Dataset):
 
     def __getitem__(self, idx):
         dic = {
-            'image_num': self.imgnums[idx],
-            'image': self.imgs[idx],
+            'names': self.imgnums[idx],
+            'images': self.imgs[idx],
             'voxels': self.gt_voxels[idx],
         }
         
@@ -192,6 +192,7 @@ class R2N2(ShapeNetBase):  # pragma: no cover
         return_all_views: bool = True,
         return_voxels: bool = False,
         return_feats: bool = False,
+        return_RTK: bool = False,
         views_rel_path: str = "ShapeNetRendering",
         voxels_rel_path: str = "ShapeNetVoxels",
         load_textures: bool = False,
@@ -213,9 +214,9 @@ class R2N2(ShapeNetBase):  # pragma: no cover
             return_feats(bool): Indicator of whether image features from a pretrained resnet18 
                 are also returned in the dataloader or not
             views_rel_path: path to rendered views within the r2n2_dir. If not specified,
-                the renderings are assumed to be at os.os.path.join(rn2n_dir, "ShapeNetRendering").
+                the renderings are assumed to be at os.path.join(rn2n_dir, "ShapeNetRendering").
             voxels_rel_path: path to rendered views within the r2n2_dir. If not specified,
-                the renderings are assumed to be at os.os.path.join(rn2n_dir, "ShapeNetVoxels").
+                the renderings are assumed to be at os.path.join(rn2n_dir, "ShapeNetVoxels").
             load_textures: Boolean indicating whether textures should loaded for the model.
                 Textures will be of type TexturesAtlas i.e. a texture map per face.
             texture_resolution: Int specifying the resolution of the texture map per face
@@ -231,12 +232,13 @@ class R2N2(ShapeNetBase):  # pragma: no cover
         self.load_textures = load_textures
         self.texture_resolution = texture_resolution
         self.return_feats = return_feats
+        self.return_RTK = return_RTK
         # Examine if split is valid.
         if split not in ["train", "val", "test"]:
             raise ValueError("split has to be one of (train, val, test).")
         # Synset dictionary mapping synset offsets in R2N2 to corresponding labels.
         with open(
-            os.os.path.join(SYNSET_DICT_DIR, "r2n2_synset_dict.json"), "r"
+            os.path.join(SYNSET_DICT_DIR, "r2n2_synset_dict.json"), "r"
         ) as read_dict:
             self.synset_dict = json.load(read_dict)
         # Inverse dictionary mapping synset labels to corresponding offsets.
@@ -248,7 +250,7 @@ class R2N2(ShapeNetBase):  # pragma: no cover
 
         self.return_images = True
         # Check if the folder containing R2N2 renderings is included in r2n2_dir.
-        if not os.os.path.isdir(os.os.path.join(r2n2_dir, views_rel_path)):
+        if not os.path.isdir(os.path.join(r2n2_dir, views_rel_path)):
             self.return_images = False
             msg = (
                 "%s not found in %s. R2N2 renderings will "
@@ -399,7 +401,7 @@ class R2N2(ShapeNetBase):  # pragma: no cover
         model["label"] = self.synset_dict[model["synset_id"]]
 
         model["images"] = None
-        images, feats, Rs, Ts, voxel_RTs = [], [], [], [], []
+        img_names, images, feats, Rs, Ts, voxel_RTs = [], [], [], [], [], []
         # Retrieve R2N2's renderings if required.
         if self.return_images:
             rendering_path = os.path.join(
@@ -413,41 +415,52 @@ class R2N2(ShapeNetBase):  # pragma: no cover
             # Read metadata file to obtain params for calibration matrices.
             with open(os.path.join(rendering_path, "rendering_metadata.txt"), "r") as f:
                 metadata_lines = f.readlines()
-            for i in model_views:
+            for i, name in enumerate(model_views):
                 # Read image.
-                image_path = os.path.join(rendering_path, "%02d.png" % i)
+                image_path = os.path.join(rendering_path, f"{name}.png")
+                # image_path = os.path.join(rendering_path, "%02d.png" % i)
                 raw_img = Image.open(image_path)
+                if raw_img.size != (137, 137):
+                    raw_img = raw_img.resize((137, 137))
+                if np.array(raw_img).ndim != 3:
+                    raw_img = cv2.cvtColor(np.array(raw_img), cv2.COLOR_GRAY2RGB)
                 image = torch.from_numpy(np.array(raw_img) / 255.0)[..., :3]
+                
+                img_names.append(int(name))
                 images.append(image.to(dtype=torch.float32))
-                feats.append(all_feats[i].to(dtype=torch.float32))
+                if self.return_feats:
+                    feats.append(all_feats[i].to(dtype=torch.float32))
 
-                # Get camera calibration.
-                azim, elev, yaw, dist_ratio, fov = [
-                    float(v) for v in metadata_lines[i].strip().split(" ")
-                ]
-                dist = dist_ratio * MAX_CAMERA_DISTANCE
-                # Extrinsic matrix before transformation to PyTorch3D world space.
-                RT = compute_extrinsic_matrix(azim, elev, dist)
-                R, T = self._compute_camera_calibration(RT)
-                Rs.append(R)
-                Ts.append(T)
-                voxel_RTs.append(RT)
+                if self.return_RTK:
+                    # Get camera calibration.
+                    azim, elev, yaw, dist_ratio, fov = [
+                        float(v) for v in metadata_lines[i].strip().split(" ")
+                    ]
+                    dist = dist_ratio * MAX_CAMERA_DISTANCE
+                    # Extrinsic matrix before transformation to PyTorch3D world space.
+                    RT = compute_extrinsic_matrix(azim, elev, dist)
+                    R, T = self._compute_camera_calibration(RT)
+                    Rs.append(R)
+                    Ts.append(T)
+                    voxel_RTs.append(RT)
 
             # Intrinsic matrix extracted from the Blender with slight modification to work with
             # PyTorch3D world space. Taken from meshrcnn codebase:
             # https://github.com/facebookresearch/meshrcnn/blob/main/shapenet/utils/coords.py
-            K = torch.tensor(
-                [
-                    [2.1875, 0.0, 0.0, 0.0],
-                    [0.0, 2.1875, 0.0, 0.0],
-                    [0.0, 0.0, -1.002002, -0.2002002],
-                    [0.0, 0.0, 1.0, 0.0],
-                ]
-            )
+            model["names"] = np.stack(img_names)
             model["images"] = torch.stack(images)
-            model["R"] = torch.stack(Rs)
-            model["T"] = torch.stack(Ts)
-            model["K"] = K.expand(len(model_views), 4, 4)
+            if self.return_RTK:
+                K = torch.tensor(
+                    [
+                        [2.1875, 0.0, 0.0, 0.0],
+                        [0.0, 2.1875, 0.0, 0.0],
+                        [0.0, 0.0, -1.002002, -0.2002002],
+                        [0.0, 0.0, 1.0, 0.0],
+                    ]
+                )
+                model["R"] = torch.stack(Rs)
+                model["T"] = torch.stack(Ts)
+                model["K"] = K.expand(len(model_views), 4, 4)
             if self.return_feats:
                 model["feats"] = torch.stack(feats)
 
@@ -484,9 +497,10 @@ class R2N2(ShapeNetBase):  # pragma: no cover
         rand_view = random.randint(0,num_views-1)
 
         model['images'] = model['images'][rand_view]
-        model['R'] = model['R'][rand_view]
-        model['T'] = model['T'][rand_view]
-        model['K'] = model['K'][rand_view]
+        if self.return_RTK:
+            model['R'] = model['R'][rand_view]
+            model['T'] = model['T'][rand_view]
+            model['K'] = model['K'][rand_view]
         if self.return_feats:
             model["feats"] = model["feats"][rand_view]
 

model.py
@@ -48,8 +48,10 @@ class Pix2Voxel(nn.Module):
         )
     
     def forward(self, images, args):
-        images_normalize = self.normalize(images)
-        # images_normalize = self.normalize(images.permute(0,3,1,2))
+        if args.r2n2:
+            images_normalize = self.normalize(images.permute(0,3,1,2))
+        else:
+            images_normalize = self.normalize(images)
         encoded_feat = self.encoder(images_normalize).squeeze(-1).squeeze(-1) # b x 512
         
         inp = encoded_feat.view(-1, 64, 2, 2, 2)

scripts/img2line.py
@@ -38,14 +38,18 @@ def parse_args():
     
     parser = argparse.ArgumentParser('Image2LineDrawing', add_help=False)
     
-    parser.add_argument('--img_dir', type=str, default='../dataset/r2n2_shapenet_dataset/r2n2')
+    # parser.add_argument('--img_dir', type=str, default='../dataset/shapenet_rotate')
+    parser.add_argument('--img_dir', type=str, default='../dataset/r2n2_shapenet_dataset/r2n2/ShapeNetRendering/03001627')
+    # parser.add_argument('--img_dir', type=str, default='../dataset/r2n2_shapenet_dataset/r2n2')
     parser.add_argument('--vox_dir', type=str, default='../dataset/r2n2_shapenet_dataset/shapenet')
-    parser.add_argument('--out_dir', type=str, default='../dataset/r2n2_shapenet_line')
+    # parser.add_argument('--out_dir', type=str, default='../dataset/shapenet_rotate_edge')
+    # parser.add_argument('--out_dir', type=str, default='../dataset/r2n2_shapenet_rotate_line')
+    parser.add_argument('--out_dir', type=str, default='../dataset/r2n2_shapenet_original_line')
     
     parser.add_argument('--sobel', action='store_true', default=False)
-    parser.add_argument('--rotate', action='store_true', default=True)
+    parser.add_argument('--rotate', action='store_true', default=False)
+    parser.add_argument('--num_rotate', type=int, default=10)
     parser.add_argument('--visualize', action='store_true', default=False)
-    parser.add_argument('--num_rotate', type=int, default=5)
     
     return parser.parse_args()
 
@@ -73,11 +77,11 @@ def get_mesh_renderer(image_size=512, lights=None, device=None):
     )
     return renderer
 
-def rotate(datadir, outdir, num_rotate=5, visualize=False):
+def rotate(datadir, outdir, num_rotate=10, visualize=False):
     voxpaths = glob(f"{datadir}/**/*.obj", recursive=True)
     
     lights = PointLights(location=[[0, 0, -3]])
-    renderer = get_mesh_renderer(image_size=224)
+    renderer = get_mesh_renderer(image_size=137)
     
     angles = torch.bernoulli(torch.rand(num_rotate*len(voxpaths), 3))
     angles *= torch.randint(0, 360, (num_rotate*len(voxpaths), 3))
@@ -86,15 +90,12 @@ def rotate(datadir, outdir, num_rotate=5, visualize=False):
     for i, voxpath in tqdm(enumerate(voxpaths), total=len(voxpaths)):
         obj_name = voxpath.split('/')[-2]
         
-        if len(glob(f"home/ubuntu/IKEA/dataset/shapenet_rotate/{obj_name}*.png")) == 5:
-            continue
+        os.makedirs(f"{outdir}/{obj_name}", exist_ok=True)
         
-        if 'f3c0ab68f3dab6071b17743c18fb63dc' in voxpath or '2ae70fbab330779e3bff5a09107428a5' in voxpath or 'a8c0ceb67971d0961b17743c18fb63dc' in voxpath:
-            continue
+        # if len(glob(f"home/ubuntu/IKEA/dataset/shapenet_rotate/{obj_name}*.png")) == 5:
+        #     continue
         
-        if not os.path.isfile(voxpath):
-            continue
-        
+        # verts, faces, _ = load_obj(voxpath)
         try:
             verts, faces, _ = load_obj(voxpath)
         except:
@@ -126,7 +127,7 @@ def rotate(datadir, outdir, num_rotate=5, visualize=False):
             rend = rend.detach().cpu().numpy()[0, ..., :3]
             rend *= 255
             
-            cv2.imwrite(f"{outdir}/{obj_name}_x{angle[0]}_y{angle[1]}_z{angle[2]}.png", rend)
+            cv2.imwrite(f"{outdir}/{obj_name}/x{angle[0]}_y{angle[1]}_z{angle[2]}.png", rend)
             
             if visualize:
                 
@@ -150,26 +151,74 @@ def rotate(datadir, outdir, num_rotate=5, visualize=False):
 
     return outdir
     
-def get_contour_and_save(imgdir, rot=None):
-    imgpaths = glob(f"{imgdir}/**/*.png", recursive=True)
+def get_contour_and_save(imgdir, args):
+    dirpaths = glob(f"{imgdir}/*", recursive=True)
+    # imgpaths = glob(f"{imgdir}/**/*.png", recursive=True)
     
-    for imgpath in imgpaths:
-        obj_name = imgpath.split('/')[-3]
-        order = imgpath.split('/')[-1]
+    # for imgpath in tqdm(imgpaths, total=len(imgpaths)):
+    #     if 'r2n2' in os.path.basename(imgdir):
+    #         obj_name = imgpath.split('/')[-3]
+    #         order = imgpath.split('/')[-1][:-4]
+    #     elif 'shapenet' in os.path.basename(imgdir):
+    #         obj_name = imgpath.split('/')[-2]
+    #         order =  imgpath.split('/')[-1][:-4]
+    #         # obj_name = os.path.basename(imgpath).split('_')[0]
+    #         # order = os.path.basename(imgpath).replace(obj_name, '')[1:-4]
+        # obj_outdir = os.path.join(args.out_dir, obj_name)
+        # os.makedirs(obj_outdir, exist_ok=True)
         
-        img = cv2.imread(imgpath)
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img_blur = cv2.GaussianBlur(img_gray, ksize=(3,3), sigmaX=cv2.BORDER_DEFAULT)
+        # img = cv2.imread(imgpath)
+        # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # img_blur = cv2.GaussianBlur(img_gray, ksize=(3,3), sigmaX=cv2.BORDER_DEFAULT)
         
-        if args.sobel:
-            sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
-            sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
-            sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
-            cv2.imwrite(f"{args.out_dir}/{obj_name}_{order}_sobel.png", sobelxy)
+        # if args.sobel:
+        #     sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+        #     sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+        #     sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+            
+        #     cv2.imwrite(os.path.join(obj_outdir, f"{order}_sobel.png"), sobelxy)
         
-        canny = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
-        canny_inv = cv2.bitwise_not(canny)
-        cv2.imwrite(f"{args.out_dir}/{obj_name}_{order}_{rot}.png", canny_inv)
+        # canny = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+        # canny_inv = cv2.bitwise_not(canny)
+        
+        # cv2.imwrite(os.path.join(obj_outdir, f"{order}.png"), canny_inv)
+    
+    skip_cnt = 0
+    for dirpath in tqdm(dirpaths, total=len(dirpaths)):
+        
+        files = glob(f"{dirpath}/**/*.png", recursive=True)
+        if len(files) < 5 :
+            skip_cnt += 1
+            continue
+        
+        # if 'shapenet' in os.path.basename(imgdir):
+        obj_name = os.path.basename(os.path.normpath(dirpath))
+        
+        obj_outdir = os.path.join(args.out_dir, obj_name)
+        os.makedirs(obj_outdir, exist_ok=True)
+        
+        if args.rotate:
+            files = files[:5]
+        
+        for imgpath in files:
+            order = os.path.basename(imgpath)[:-4]
+            
+            img = cv2.imread(imgpath)
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_blur = cv2.GaussianBlur(img_gray, ksize=(3,3), sigmaX=cv2.BORDER_DEFAULT)
+            
+            if args.sobel:
+                sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+                sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+                sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+                
+                cv2.imwrite(os.path.join(obj_outdir, f"{order}_sobel.png"), sobelxy)
+            
+            canny = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+            canny_inv = cv2.bitwise_not(canny)
+            
+            cv2.imwrite(os.path.join(obj_outdir, f"{order}.png"), canny_inv)
+    print("@@@ skip_cnt: ", skip_cnt)
 
 def main(args):
     
@@ -185,7 +234,7 @@ def main(args):
     else:
         imgdir = args.img_dir
     
-    get_contour_and_save(imgdir, rot=None)
+    get_contour_and_save(imgdir, args)
     
 
 if __name__ == "__main__":

train_3drecon.py
@@ -4,6 +4,9 @@ import time
 import argparse
 from tqdm import tqdm
 from datetime import datetime
+import h5py
+import warnings
+warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
 
 import numpy as np
 import torch
@@ -11,15 +14,15 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from torchvision import models as torchvision_models
 import pytorch3d
-import h5py
+from  pytorch3d.datasets.r2n2.utils import collate_batched_R2N2
 
 from model import Pix2Voxel
 from train_utils import create_dir
-from data_loader import IKEAManualStep
+from data_loader import IKEAManualStep, R2N2
 
 def parse_args():
     
@@ -29,8 +32,12 @@ def parse_args():
     parser.add_argument('--encoder_arch', default='resnet18', type=str, help="architecture for encoder")
     parser.add_argument('--model', default='pix2vox', type=str, help="architecture for 3D reconstruction")
     
+    # Pre-Training parameters
+    parser.add_argument('--r2n2', default=True, action='store_true')
+    parser.add_argument('--r2n2_dir', default='./dataset/r2n2_shapenet_dataset', type=str)
+    
     # Training parameters
-    parser.add_argument('--num_epochs', default=10000, type=int)
+    parser.add_argument('--max_iter', default=10000, type=int)
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--num_workers', default=2, type=int)
     parser.add_argument('--lr', default=4e-4, type=str)
@@ -48,7 +55,7 @@ def parse_args():
     parser.add_argument('--device', default='cuda', type=str) 
     
     # Directories & Checkpoint
-    parser.add_argument('--load_checkpoint', default=None, type=str)            
+    parser.add_argument('--load_checkpoint', default='./checkpoints/pix2vox/checkpoint_2000.pth', type=str)            
     parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints')
     parser.add_argument('--logs_dir', type=str, default='./logs')
     parser.add_argument('--dataset_path', type=str, default='../dataset')
@@ -56,10 +63,15 @@ def parse_args():
     return parser.parse_args()
 
 def preprocess(feed_dict, args):
-    #TODO: adjust the way data are loaded & formatted
-    image_names = torch.tensor(feed_dict['image_num']).reshape(-1, 1)
-    images = feed_dict['image'].squeeze(1)
-    voxels = feed_dict['voxels'].float()
+    if args.r2n2:
+        # image_names = torch.tensor(0) #FIXME
+        image_names = torch.tensor(np.array(feed_dict['names'])).reshape(-1, 1)
+        images = feed_dict['images']
+        voxels = feed_dict['voxels'].squeeze(1).float()
+    else:
+        image_names = torch.tensor(feed_dict['names']).reshape(-1, 1)
+        images = feed_dict['images'].squeeze(1)
+        voxels = feed_dict['voxels'].float()
     ground_truth_3d = voxels
     
     return image_names.to(args.device), images.to(args.device), ground_truth_3d.to(args.device)
@@ -94,9 +106,11 @@ def main(args):
         checkpoint = torch.load(args.load_checkpoint)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        start_iter = checkpoint['epoch']
-        print(f"Succesfully loaded iter {start_iter}")
-    
+        start_iter = checkpoint['step']
+        print(f"@@@@ Succesfully loaded iter {start_iter}")
+    else:
+        start_iter = 0
+        
     ## Load dataset
     transform = transforms.Compose([
         transforms.Resize(tuple((args.resize_w, args.resize_h))),
@@ -104,31 +118,65 @@ def main(args):
     ]) #TODO: normalize?
     args.transforms = transform
 
-    dataset = IKEAManualStep(args)
-    train_set, test_set = torch.utils.data.random_split(dataset, [args.train_test_split_ratio, 1-args.train_test_split_ratio])
+    if args.r2n2:
+        shapenet_path = f"{args.r2n2_dir}/shapenet"
+        r2n2_path = f"{args.r2n2_dir}/r2n2"
+        splits_path = f"{args.r2n2_dir}/line_split.json"
+        
+        train_set = R2N2("train", 
+                            shapenet_path, r2n2_path, splits_path, 
+                            return_voxels=True, return_feats=False, return_RTK=False,
+                            views_rel_path="LineDrawings")
+        test_set = R2N2("test", 
+                            shapenet_path, r2n2_path, splits_path, 
+                            return_voxels=True, return_feats=False, return_RTK=False,
+                            views_rel_path="LineDrawings")
 
-    train_dataloader = DataLoader(dataset=train_set, 
-                              batch_size=args.batch_size, 
-                              shuffle=True, 
-                              num_workers=args.num_workers)
+        train_dataloader = DataLoader(
+            train_set,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            collate_fn=collate_batched_R2N2,
+            pin_memory=True,
+            drop_last=True,
+            )
+        test_dataloader = DataLoader(
+            test_set,
+            batch_size=1,
+            num_workers=args.num_workers,
+            collate_fn=collate_batched_R2N2,
+            pin_memory=True,
+            drop_last=True,
+            )
+        train_loader = iter(train_dataloader)
+        test_loader = test_dataloader
+        
+    else:
+        dataset = IKEAManualStep(args)
+        train_set, test_set = random_split(dataset, [args.train_test_split_ratio, 1-args.train_test_split_ratio])
 
-    test_dataloader = DataLoader(dataset=test_set, 
-                            batch_size=args.batch_size, 
-                            shuffle=True, 
-                            num_workers=args.num_workers)
+        train_dataloader = DataLoader(dataset=train_set, 
+                                batch_size=args.batch_size, 
+                                shuffle=True, 
+                                num_workers=args.num_workers)
 
-    train_loader = iter(train_dataloader)
-    test_loader = test_dataloader
+        test_dataloader = DataLoader(dataset=test_set, 
+                                batch_size=args.batch_size, 
+                                shuffle=True, 
+                                num_workers=args.num_workers)
+
+        train_loader = iter(train_dataloader)
+        test_loader = test_dataloader
     
-    print (f"Successfully loaded data: train_set {len(train_set)} | test_set {len(test_set)}")
-    print("Starting training!")
+    print (f"@@@@ Successfully loaded data: train_set {len(train_set)} | test_set {len(test_set)}")
     
-    start_epoch = 0
+    print("@@@@ Starting training!")
+    
     start_time = time.time()
-    for epoch in range(start_epoch, args.num_epochs):
-        epoch_start_time = time.time()
+    for step in range(start_iter, args.max_iter):
+        step_start_time = time.time()
 
-        if epoch % len(train_loader) == 0: #restart after one epoch
+        if step % len(train_loader) == 0: #restart after one step
             train_loader = iter(train_dataloader)
 
         read_start_time = time.time()
@@ -147,22 +195,24 @@ def main(args):
         optimizer.step()        
 
         total_time = time.time() - start_time
-        iter_time = time.time() - epoch_start_time
+        iter_time = time.time() - step_start_time
 
         loss_vis = loss.cpu().item()
+        
+        writer.add_scalar('train_loss', loss_vis, step)
 
-        if ((epoch + 1) % args.save_freq) == 0:
-            print("saved at ", epoch)
+        if ((step + 1) % args.save_freq) == 0:
+            print("saved at ", step)
             torch.save({
-                'epoch': epoch,
+                'step': step,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()
-                }, os.path.join(args.checkpoint_dir, 'checkpoint_{}.pth'.format(epoch)))
+                }, os.path.join(args.checkpoint_dir, 'checkpoint_{}.pth'.format(step+1)))
 
-        print("[%4d/%4d]; ttime: %.0f (%.2f, %.2f); loss: %.3f" % (epoch, args.num_epochs, total_time, read_time, iter_time, loss_vis))
+        print("[%4d/%4d]; ttime: %.0f (%.2f, %.2f); loss: %.3f" % (step, args.max_iter, total_time, read_time, iter_time, loss_vis))
 
-        if ((epoch + 1) % args.eval_freq) == 0:
-            print("Evaluating at ", epoch)
+        if ((step + 1) % args.eval_freq) == 0:
+            print("Evaluating at ", step)
             model.eval()
             with torch.no_grad():
                 num_batches = 0
@@ -170,7 +220,8 @@ def main(args):
                 predictions = None
                 image_names = None
                 for batch in test_loader:
-                    image_test_names, images_test, ground_truth_3d = preprocess(feed_dict, args)
+                    image_test_names, images_test, ground_truth_3d = preprocess(batch, args)
+                    # image_test_names, images_test, ground_truth_3d = preprocess(feed_dict, args)
                     prediction_3d = model(images_test, args).squeeze()
                     if predictions is None:
                         predictions = prediction_3d
@@ -179,13 +230,15 @@ def main(args):
                         predictions = torch.cat([predictions, prediction_3d], dim=0)
                         image_names = torch.cat([image_names, image_test_names], dim=0)
                     num_batches += 1
-                    total_loss += calculate_voxel_loss(prediction_3d, ground_truth_3d).cpu().item()
+                    total_loss += calculate_voxel_loss(prediction_3d, ground_truth_3d.squeeze()).cpu().item()
 
                 if num_batches != 0:
                     total_loss /= num_batches
-                print("Loss at epoch {}: {}".format(epoch, total_loss))
-                np.savetxt(os.path.join(args.logs_dir, dt, 'output_{:05d}_names.txt'.format(epoch)), image_names.cpu().numpy(), fmt="%d")
-                output_file = h5py.File(os.path.join(args.logs_dir, dt, 'output_{:05d}.h5'.format(epoch)), 'w')
+                print("Loss at step {}: {}".format(step, total_loss))
+                writer.add_scalar("eval_loss", total_loss, step)
+                
+                np.savetxt(os.path.join(args.logs_dir, dt, 'output_{:05d}_names.txt'.format(step)), image_names.cpu().numpy(), fmt="%d")
+                output_file = h5py.File(os.path.join(args.logs_dir, dt, 'output_{:05d}.h5'.format(step)), 'w')
                 output_file.create_dataset('tensor', data=predictions.cpu().numpy())
                 output_file.close()
         model.train()