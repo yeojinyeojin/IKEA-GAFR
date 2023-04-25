c65718944e62fbad55a540a150c3f7b9b9d9009c
Yeojin Jung <yj1254@nyu.edu>
2023-04-25 00:58:34 +0000

    created a custom dataset that uses segmented density map and raw pointcloud as gt

--- sketchsampler/src/data/sketch_points/dataset.py
+++ sketchsampler/src/data/sketch_points/dataset.py
@@ -28,30 +28,88 @@ class DataSet(Dataset):
                     break
                 self.pkl_list.append(line)
 
-    def __len__(self) -> int:
+    def __len__(self):
         return len(self.pkl_list)
 
-    def __getitem__(
-            self, idx
-    ) -> Any:
+    def __getitem__(self, idx):
         camera_path = os.path.join(self.path_camera, self.pkl_list[idx])
         density_path = os.path.join(self.path_density, self.pkl_list[idx])
         pointcloud_path = os.path.join(self.path_pt, "/".join(self.pkl_list[idx].split('/')[:2] + ["pt.dat"]))
+        
         raw_pointcloud = pkl.load(open(pointcloud_path, 'rb'))
         cam_mat, cam_pos = pkl.load(open(camera_path, 'rb'))
+        
         density_map = pkl.load(open(density_path, 'rb')).astype('float32')
         density_map /= np.sum(density_map)
         density_map = density_map[None, ...]
+        
         gt_pointcloud = np.dot(raw_pointcloud - cam_pos, cam_mat.transpose())
         gt_pointcloud[:, 2] -= np.mean(gt_pointcloud[:, 2])
+        
         sketch_path = os.path.join(self.path_sketch, self.pkl_list[idx].replace('.dat', '.png'))
         sketch = io.imread(sketch_path)
         sketch[np.where(sketch[:, :, 3] == 0)] = 255
         sketch = sketch.astype('float32') / 255
         sketch = ((np.transpose(rgb2gray(sketch[:, :, :3]), (2, 0, 1)) - .5) * 2).astype('float32')
+        
         metadata = self.pkl_list[idx][:-4]
+        
         item = (sketch, gt_pointcloud, density_map, metadata)
         return item
 
-    def __repr__(self) -> str:
+    def __repr__(self):
+        return f"Dataset(name={self.name!r}"
+    
+class CustomDataSet(Dataset):
+    def __init__(self, name: ValueNode, path_sketch: str, path_pt: str, path_camera: str,
+                 path_density_seg: str, file_list: str, cfg: DictConfig, **kwargs):
+        super().__init__()
+        self.cfg = cfg
+        self.name = name
+
+        self.path_sketch = path_sketch
+        self.path_pt = path_pt
+        self.path_camera = path_camera
+        self.path_density_seg = path_density_seg
+        self.pkl_list = []
+        with open(file_list, 'r') as f:
+            while (True):
+                line = f.readline().strip()
+                if not line:
+                    break
+                self.pkl_list.append(line)
+
+    def __len__(self):
+        return len(self.pkl_list)
+
+    def __getitem__(self, idx):
+        camera_path = os.path.join(self.path_camera, self.pkl_list[idx])
+        seg_density_path = os.path.join(self.path_density_seg, self.pkl_list[idx])
+        pointcloud_path = os.path.join(self.path_pt, "/".join(self.pkl_list[idx].split('/')[:2] + ["pt.dat"]))
+        
+        raw_pointcloud = pkl.load(open(pointcloud_path, 'rb'))
+        cam_mat, cam_pos = pkl.load(open(camera_path, 'rb'))
+        
+        # density_map = pkl.load(open(density_path, 'rb')).astype('float32')
+        # density_map /= np.sum(density_map)
+        # density_map = density_map[None, ...]
+        seg_density_map = pkl.load(open(seg_density_path, 'rb')).astype('float32')
+        
+        ## use canonical view instead of object-centric view
+        gt_pointcloud = raw_pointcloud
+        # gt_pointcloud = np.dot(raw_pointcloud - cam_pos, cam_mat.transpose())
+        # gt_pointcloud[:, 2] -= np.mean(gt_pointcloud[:, 2])
+        
+        sketch_path = os.path.join(self.path_sketch, self.pkl_list[idx].replace('.dat', '.png'))
+        sketch = io.imread(sketch_path)
+        sketch[np.where(sketch[:, :, 3] == 0)] = 255
+        sketch = sketch.astype('float32') / 255
+        sketch = ((np.transpose(rgb2gray(sketch[:, :, :3]), (2, 0, 1)) - .5) * 2).astype('float32')
+        
+        metadata = self.pkl_list[idx][:-4]
+        
+        item = (sketch, gt_pointcloud, seg_density_map, metadata)
+        return item
+
+    def __repr__(self):
         return f"Dataset(name={self.name!r}"
